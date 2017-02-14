1e4095c31b7c9dd0bcb867f30049c39543d5276d
Gaurav Sanghani <g@sanghani.me>
2017-02-14 01:23:55 -0600

    Fleshes out the wedding section. Comments out all other sections for now.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (function () {
+    // Animate scrolling from navbar
     $('.navbar a').click(function(event) {
 	event.preventDefault();
 	
@@ -9,6 +10,17 @@
 	      target_top = target_offset.top;
 	
 	$('html, body').animate({scrollTop:target_top}, 500);
-	
     });
+
+    // Compute the # of months from now til the wedding!
+    function monthsTil(date) {
+	const now = new Date();
+	const years = date.getYear() - now.getYear();
+	const months = date.getMonth() - now.getMonth();
+	return years * 12 + months;
+    }
+
+    const WEDDING_DATE = new Date(1517464800000); //February 01, 2018 T00:00:00.000
+    $('.time-to-wedding div').text(monthsTil(WEDDING_DATE));
+    window.monthsTil = monthsTil;
 })();
