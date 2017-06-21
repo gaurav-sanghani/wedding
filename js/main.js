7966376ec28c48a84aee0f21348209ff6ca0ebea
Gaurav Sanghani <g@sanghani.me>
2017-06-21 01:39:11 -0500

    Add specific wedding dates. Change from month to day counter.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
     $('.navbar a').click(function(event) {
 	event.preventDefault();
 	
-	const full_url = this.href,
+	var full_url = this.href,
 	      parts = full_url.split('#'),
 	      trgt = parts[1],
 	      target_offset = $('#'+trgt).offset(),
@@ -13,14 +13,28 @@
     });
 
     // Compute the # of months from now til the wedding!
-    function monthsTil(date) {
-	const now = new Date();
-	const years = date.getYear() - now.getYear();
-	const months = date.getMonth() - now.getMonth();
-	return years * 12 + months;
+    function daysTil(startTs, endTs) {
+	return ((endTs - startTs) / (1000 * 60 * 60 * 24)) | 0; // force to int
     }
 
-    const WEDDING_DATE = new Date(1517464800000); //February 01, 2018 T00:00:00.000
-    $('.time-to-wedding div').text(monthsTil(WEDDING_DATE));
-    window.monthsTil = monthsTil;
+    var WEDDING_START_TS = 1518760800000; //February 16, 2018 T00:00:00.000 CST
+    var WEDDING_END_TS = 1518933599000; // February 17, 2018 T23:59:59 CST
+    var now = (new Date()).getTime();
+    var days = daysTil(now, WEDDING_START_TS);
+    var counter = $('.time-to-wedding div');
+    var countdownHeader = $('.countdown-header');
+    if (days > 0) {
+	counter.text(days);
+    } else {
+	days = daysTil(now, WEDDING_END_TS);
+	if (days < 0) {
+	    counter.text(days * -1);
+	    countdownHeader.text('Days since February 17, 2018');
+	} else {
+	    countdownHeader.text('It is ' + (new Date()).toDateString());
+	    counter.text('Today is the wedding!');
+	    counter.removeClass('countdown');
+	}
+    }
+    window.daysTil = daysTil;
 })();
