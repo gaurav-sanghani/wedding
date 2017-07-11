c0fda9256433492f5115e34712b214ae44e10a02
Gaurav Sanghani <g@sanghani.me>
2017-07-10 23:31:42 -0500

    Countdown to only Feb 17.

--- js/main.js
+++ js/main.js
@@ -17,24 +17,21 @@
 	return ((endTs - startTs) / (1000 * 60 * 60 * 24)) | 0; // force to int
     }
 
-    var WEDDING_START_TS = 1518760800000; //February 16, 2018 T00:00:00.000 CST
-    var WEDDING_END_TS = 1518933599000; // February 17, 2018 T23:59:59 CST
+    var WEDDING_START_TS = 1518847200000; //February 17, 2018 T00:00:00.000 CST
     var now = (new Date()).getTime();
     var days = daysTil(now, WEDDING_START_TS);
     var counter = $('.time-to-wedding div');
     var countdownHeader = $('.countdown-header');
     if (days > 0) {
 	counter.text(days);
+    } else if (days < 0) {
+	counter.text(days * -1);
+	countdownHeader.text("Days we've been married");
     } else {
-	days = daysTil(now, WEDDING_END_TS);
-	if (days < 0) {
-	    counter.text(days * -1);
-	    countdownHeader.text('Days since February 17, 2018');
-	} else {
-	    countdownHeader.text('It is ' + (new Date()).toDateString());
-	    counter.text('Today is the wedding!');
-	    counter.removeClass('countdown');
-	}
+	countdownHeader.text('It is ' + (new Date()).toDateString());
+	counter.text('Today is the wedding!');
+	counter.removeClass('countdown');
     }
+
     window.daysTil = daysTil;
 })();
